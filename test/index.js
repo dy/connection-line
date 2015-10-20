dce8d3c47b1c95e1faf784c95984530916e873d3
Dmitry <df.creative@gmail.com>
2015-10-20 11:58:26 -0400

    Outline tests

---
+++ test/index.js
@@ -0,0 +1,25 @@
+/**
+ * connection-line tests/demo page
+ */
+
+describe('Connection-line tests', function () {
+	it('Start == end', function () {
+
+	});
+
+	it('Line marks', function () {
+
+	});
+
+	it('Curvature', function () {
+
+	});
+
+	it('Swap channels', function () {
+
+	});
+
+	it('Multichannels', function () {
+
+	});
+});
